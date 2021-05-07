8cd7717e4ad72c6928400c59e145a419c74f06d1
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-07 12:14:13 +0300

    urun listesi icin html sablonu olusuturldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -5,4 +5,43 @@
  */
 ?>
 
-ürün listesi
+<div class="row">
+
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürünler</h4>
+    </div><!-- END column -->
+
+    <div class="col-md-12">
+        <div class="widget p-lg">
+
+            <table class="table table-hover table-striped">
+               <thead>
+                    <th>#Id</th>
+                    <th>Url</th>
+                    <th>Başlık</th>
+                    <th>Açıklama</th>
+                    <th>Durumu</th>
+                    <th>İşlem</th>
+               </thead>
+                <tbody>
+                <tr>
+                    <td>#1</td>
+                    <td>#1</td>
+                    <td>#1</td>
+                    <td>#1</td>
+                    <td>#1</td>
+                    <td>
+                        <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash">Sil</i></a>
+                        <a href="#" class="btn btn-sm btn-info btn-outline"><i class="fa fa-pencil-square-o">Düzenle</i></a>
+
+
+
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+
+</div><!-- .row -->

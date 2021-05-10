5b5517c72c93c6f1da3ebc35510a887ac28d4928
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-10 11:16:01 +0300

    sweet alert entegre edildi ve custum css olusturuldu

---
+++ panel/assets/assets/js/custom.js
@@ -0,0 +1,19 @@
+/**
+ * User: Mustafa Emre Ilhan
+ * Job: Junior Software Developer */
+
+/* Döküman hazır oldugu zaman bu calısacak
+$(document).ready(function () {
+alert();
+})
+ */
+
+
+$(document).ready(function () {
+        swal.fire({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+    })
+})

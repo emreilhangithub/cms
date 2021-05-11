e310d443378db3a018d8dca122ae94d0d0c837e3
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-11 11:16:41 +0300

    <page style olusturuldu ve css dinamik hale getirildi>

---
+++ panel/application/logs/log-2021-05-11.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-05-11 10:07:17 --> 404 Page Not Found: Assets/css
+ERROR - 2021-05-11 10:07:44 --> 404 Page Not Found: Assets/css
---
+++ panel/application/views/product_v/image/page_style.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet"
+      href="<?php echo base_url("assets/assets/css/{$viewFolder}_{$subViewFolder}_style.css"); ?>
+">
+<!-- burada sunu sagladık floder adı product sub view adı image ondan dolayı bu şekilde yazdık-->
